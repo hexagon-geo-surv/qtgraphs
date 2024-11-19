c0284da019413b45071030d4e75314ed70e7d0cd
Tomi Korpipaa <tomi.korpipaa@qt.io>
2024-11-19 13:34:02 +0200

    Fix cmake and qmake instructions for widget items
    Pick-to: 6.8.1
    Fixes: QTBUG-131366
    Change-Id: I694cfe262e31593010bc03ac56748680e7996359
    Reviewed-by: Alexei Cazacov <alexei.cazacov@qt.io>
    Reviewed-by: Sami Varanka <sami.varanka@qt.io>
    (cherry picked from commit 0a2ff2356fdea8500f3eb0ed4741ec8a6f6b34e7)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/graphs3d/widget/q3dsurfacewidgetitem.cpp
+++ src/graphs3d/widget/q3dsurfacewidgetitem.cpp
@@ -9,7 +9,7 @@ QT_BEGIN_NAMESPACE
 
 /*!
  * \class Q3DSurfaceWidgetItem
- * \inmodule QtGraphs
+ * \inmodule QtGraphsWidgets
  * \ingroup graphs_3D_widgets
  * \brief The Q3DSurfaceWidgetItem class provides methods for rendering 3D surface plots.
  *
