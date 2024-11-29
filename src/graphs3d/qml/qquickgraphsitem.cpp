69ee9ed34248d0b1d7592dce26cd71f3feb9b4a3
Sami Varanka <sami.varanka@qt.io>
2024-11-29 12:10:15 +0200

    Don't set background rotation every time
    Fixes: QTBUG-131736
    Change-Id: I31c6a1646e7037df5bc5cef9e116d858ccdd56ae
    Reviewed-by: Tomi Korpipää <tomi.korpipaa@qt.io>
    Reviewed-by: Sami Varanka <sami.varanka@qt.io>
    Reviewed-by: Sakaria Pouke <sakaria.pouke@qt.io>
    (cherry picked from commit 682079384b30f6e9d4f282b470c67c6db51fc8be)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/graphs3d/qml/qquickgraphsitem.cpp
+++ src/graphs3d/qml/qquickgraphsitem.cpp
@@ -2477,20 +2477,23 @@ void QQuickGraphsItem::synchData()
     }
 
     auto rotation = Utils::calculateRotation(rotVec);
-    if (m_yFlipped) {
-        m_backgroundRotation->setRotation(rotation);
-        if (m_axisX->labelAutoAngle() > 0.0f ||
-                m_axisY->labelAutoAngle() > 0.0f ||
-                m_axisZ->labelAutoAngle() > 0.0f) {
-            m_labelsNeedupdate = true;
-        }
-    } else {
-        modelMatrix.rotate(rotation);
-        m_backgroundRotation->setRotation(rotation);
-        if (m_axisX->labelAutoAngle() > 0.0f ||
-                m_axisY->labelAutoAngle() > 0.0f ||
-                m_axisZ->labelAutoAngle() > 0.0f) {
-            m_labelsNeedupdate = true;
+
+    if (rotation != m_backgroundRotation->rotation()) {
+        if (m_yFlipped) {
+            m_backgroundRotation->setRotation(rotation);
+            if (m_axisX->labelAutoAngle() > 0.0f ||
+                    m_axisY->labelAutoAngle() > 0.0f ||
+                    m_axisZ->labelAutoAngle() > 0.0f) {
+                m_labelsNeedupdate = true;
+            }
+        } else {
+            modelMatrix.rotate(rotation);
+            m_backgroundRotation->setRotation(rotation);
+            if (m_axisX->labelAutoAngle() > 0.0f ||
+                    m_axisY->labelAutoAngle() > 0.0f ||
+                    m_axisZ->labelAutoAngle() > 0.0f) {
+                m_labelsNeedupdate = true;
+            }
         }
     }
 
